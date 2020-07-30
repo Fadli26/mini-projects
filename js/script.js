843b82e1a7b61bc15fbf9092e155b7cf086be616
asharifauzan <fauzansaja10@gmail.com>
2020-07-30 20:39:37 +0700

    mengisi for di label dan id di input secara otomatis

--- js/script.js
+++ js/script.js
@@ -2,41 +2,48 @@ const container = document.querySelector(".container");
 const keyword = document.querySelector(".keyword");
 const list = document.querySelector(".kotak ul");
 
-function createElement() {
-    let liBaru = document.createElement("li");
-    let textKeyword = document.createTextNode(keyword.value);
-    let spankeyword = document.createElement("span");
-    let txtClose = document.createTextNode("X");
-    let checkbox = document.createElement("input");
-    let close = document.createElement("span");
-    checkbox.setAttribute("type", "checkbox");
-    checkbox.setAttribute("class", "check");
-    close.setAttribute("class", "close");
-    close.appendChild(txtClose);
-    liBaru.appendChild(checkbox);
-    spankeyword.appendChild(textKeyword);
-    liBaru.appendChild(spankeyword);
-    liBaru.appendChild(close);
-    list.appendChild(liBaru);
+// get current id of list. id = index of list
+let currentId = list.childElementCount;
+
+function createElement(id) {
+  let liBaru = document.createElement("li");
+  let textKeyword = document.createTextNode(keyword.value);
+  let labelkeyword = document.createElement("label");
+  let txtClose = document.createTextNode("X");
+  let checkbox = document.createElement("input");
+  let close = document.createElement("span");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.setAttribute("class", "check");
+  checkbox.setAttribute("id", id);
+  labelkeyword.setAttribute("for", id)
+  close.setAttribute("class", "close");
+  close.appendChild(txtClose);
+  liBaru.appendChild(checkbox);
+  labelkeyword.appendChild(textKeyword);
+  liBaru.appendChild(labelkeyword);
+  liBaru.appendChild(close);
+  list.appendChild(liBaru);
+
+  currentId++;
 }
 
-container.addEventListener("click", function (e) {
-    if (e.target.className == "btn") {
-        if (keyword.value == "") {
-            alert("masukan keyword!");
-        } else {
-            createElement();
-            keyword.value = "";
-        }
-    } else if (e.target.className == "close") {
-        e.target.parentElement.style.display = "none";
-    } else if (e.target.className == "check") {
-        if (e.target.checked) {
-            e.target.nextElementSibling.classList.add("remove");
-        } else {
-            e.target.nextElementSibling.classList.remove("remove");
-        }
-    } else if (e.target.className == "clear") {
-        e.target.previousElementSibling.innerHTML = "";
+container.addEventListener("click", function(e) {
+  if (e.target.className == "btn") {
+    if (keyword.value == "") {
+      alert("masukan keyword!");
+    } else {
+      createElement(currentId);
+      keyword.value = "";
     }
+  } else if (e.target.className == "close") {
+    e.target.parentElement.style.display = "none";
+  } else if (e.target.className == "check") {
+    if (e.target.checked) {
+      e.target.nextElementSibling.classList.add("remove");
+    } else {
+      e.target.nextElementSibling.classList.remove("remove");
+    }
+  } else if (e.target.className == "clear") {
+    e.target.previousElementSibling.innerHTML = "";
+  }
 });
